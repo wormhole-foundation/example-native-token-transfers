da7c8031bf2533df79ae5999d23f58b1d1e74423
Nikhil Suri <nikhilsuri@comcast.net>
2024-01-11 22:28:00 -0800

    evm: Refactor to flexibly use standalone or combined Endpoint and EndpointManager contracts

--- evm/src/Endpoint.sol
+++ evm/src/Endpoint.sol
@@ -1,36 +1,11 @@
 // SPDX-License-Identifier: Apache 2
 pragma solidity >=0.6.12 <0.9.0;
 
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
-
 import "./interfaces/IEndpoint.sol";
 
-abstract contract Endpoint is IEndpoint, Ownable {
-    /// updating bridgeManager requires a new Endpoint deployment.
-    /// Projects should implement their own governance to remove the old Endpoint contract address and then add the new one.
-    address immutable manager;
+abstract contract Endpoint is IEndpoint {
     // Mapping of siblings on other chains
-    mapping(uint16 => bytes32) siblings;
-    // TODO -- Add state to prevent messages from being double-submitted. Could be VAA hash and Axelar equivalent? Or could hash the entire EndpointMessage (but need unique fields like blocknum and timestamp then).
-
-    modifier onlyManager() {
-        if (msg.sender != manager) {
-            revert CallerNotManager(msg.sender);
-        }
-        _;
-    }
-
-    constructor(address _manager) {
-        manager = _manager;
-    }
-
-    /// @notice Called by the BridgeManager contract to send a cross-chain message.
-    function sendMessage(
-        uint16 recipientChain,
-        bytes memory payload
-    ) external payable onlyManager {
-        _sendMessage(recipientChain, payload);
-    }
+    mapping(uint16 => bytes32) _siblings;
 
     function _sendMessage(
         uint16 recipientChain,
@@ -38,27 +13,33 @@ abstract contract Endpoint is IEndpoint, Ownable {
     ) internal virtual;
 
     /// @notice Receive an attested message from the verification layer
-    ///         This function should verify the encodedVm and then call attestationReceived on the bridge manager contract.
-    function receiveMessage(bytes memory encodedMessage) external virtual;
+    ///         This function should verify the encodedVm and then call attestationReceived on the endpoint manager contract.
+    function receiveMessage(bytes memory encodedMessage) external {
+        bytes memory payload = _verifyMessage(encodedMessage);
+        _deliverToManager(payload);
+    }
 
-    function quoteDeliveryPrice(
+    function _verifyMessage(
+        bytes memory encodedMessage
+    ) internal virtual returns (bytes memory);
+
+    function _deliverToManager(bytes memory payload) internal virtual;
+
+    function _quoteDeliveryPrice(
         uint16 targetChain
-    ) external view virtual returns (uint256 nativePriceQuote);
+    ) internal view virtual returns (uint256);
 
     /// @notice Get the corresponding Endpoint contract on other chains that have been registered via governance.
     ///         This design should be extendable to other chains, so each Endpoint would be potentially concerned with Endpoints on multiple other chains
     ///         Note that siblings are registered under wormhole chainID values
     function getSibling(uint16 chainId) public view returns (bytes32) {
-        return siblings[chainId];
+        return _siblings[chainId];
     }
 
-    function setSibling(
-        uint16 chainId,
-        bytes32 bridgeContract
-    ) external onlyOwner {
-        if (bridgeContract == bytes32(0)) {
+    function _setSibling(uint16 chainId, bytes32 siblingContract) internal {
+        if (siblingContract == bytes32(0)) {
             revert InvalidSiblingZeroAddress();
         }
-        siblings[chainId] = bridgeContract;
+        _siblings[chainId] = siblingContract;
     }
 }
---
+++ evm/src/interfaces/IEndpointStandalone.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache 2
+pragma solidity >=0.6.12 <0.9.0;
+
+interface IEndpointStandalone {
+    error CallerNotManager(address caller);
+
+    function quoteDeliveryPrice(
+        uint16 recipientChain
+    ) external view returns (uint256);
+
+    function sendMessage(
+        uint16 recipientChain,
+        bytes memory payload
+    ) external payable;
+
+    function setSibling(uint16 chainId, bytes32 siblingContract) external;
+}
