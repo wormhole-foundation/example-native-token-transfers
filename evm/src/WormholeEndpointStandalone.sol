bf4da1b2329fc9b96f329008fa91e914a0acdc09
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2024-01-13 14:07:16 +0000

    evm: WormholeEndpointStandalone: add TODO about Ownable

--- evm/src/WormholeEndpointStandalone.sol
+++ evm/src/WormholeEndpointStandalone.sol
@@ -6,6 +6,7 @@ import "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "./WormholeEndpoint.sol";
 import "./EndpointStandalone.sol";
 
+// TODO: we shouldn't use Ownable from openzeppelin as it uses a non-deterministic storage slot
 contract WormholeEndpointStandalone is WormholeEndpoint, EndpointStandalone, Ownable {
     constructor(
         address manager,
