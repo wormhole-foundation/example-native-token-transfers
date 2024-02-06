d0ef2df1b52b5e05d5c6e004e0c941955c755847
Nikhil Suri <nikhilsuri@comcast.net>
2024-02-06 12:06:17 -0800

    Pull out rate limiting logic into abstract contract (#60)

--- src/interfaces/IManager.sol
+++ src/interfaces/IManager.sol
@@ -10,36 +10,11 @@ interface IManager {
     error InvalidTargetChain(uint16 targetChain, uint16 thisChain);
     error ZeroAmount();
     error InvalidAddressLength(uint256 length);
-    error NotEnoughCapacity(uint256 currentCapacity, uint256 amount);
     error InvalidMode(uint8 mode);
-    error OutboundQueuedTransferNotFound(uint64 queueSequence);
-    error OutboundQueuedTransferStillQueued(uint64 queueSequence, uint256 transferTimestamp);
-    error InboundQueuedTransferNotFound(bytes32 digest);
-    error InboundQueuedTransferStillQueued(bytes32 digest, uint256 transferTimestamp);
     error InvalidSibling(uint16 chainId, bytes siblingAddress);
     error InvalidSiblingChainIdZero();
     error InvalidSiblingZeroLength();
     error InvalidSiblingZeroBytes();
-
-    struct RateLimitParams {
-        uint256 limit;
-        uint256 currentCapacity;
-        uint256 ratePerSecond;
-        uint64 lastTxTimestamp;
-    }
-
-    struct OutboundQueuedTransfer {
-        uint256 amount;
-        bytes32 recipient;
-        uint64 txTimestamp;
-        uint16 recipientChain;
-    }
-
-    struct InboundQueuedTransfer {
-        uint256 amount;
-        uint64 txTimestamp;
-        address recipient;
-    }
 
     function transfer(
         uint256 amount,
@@ -55,29 +30,11 @@ interface IManager {
 
     function completeInboundQueuedTransfer(bytes32 digest) external;
 
-    function quoteDeliveryPrice(uint16 recipientChain) external view returns (uint256);
-
     function setOutboundLimit(uint256 limit) external;
-
-    function getOutboundLimitParams() external view returns (RateLimitParams memory);
-
-    function getCurrentOutboundCapacity() external view returns (uint256);
-
-    function getOutboundQueuedTransfer(uint64 queueSequence)
-        external
-        view
-        returns (OutboundQueuedTransfer memory);
 
     function setInboundLimit(uint256 limit, uint16 chainId) external;
 
-    function getInboundLimitParams(uint16 chainId) external view returns (RateLimitParams memory);
-
-    function getCurrentInboundCapacity(uint16 chainId) external view returns (uint256);
-
-    function getInboundQueuedTransfer(bytes32 digest)
-        external
-        view
-        returns (InboundQueuedTransfer memory);
+    function quoteDeliveryPrice(uint16 recipientChain) external view returns (uint256);
 
     function nextMessageSequence() external view returns (uint64);
 
