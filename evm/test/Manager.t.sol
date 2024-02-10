9c77efdbbcc17b0cc4c139f84eb46d42641bd785
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2024-02-10 18:22:25 +0000

    evm: EndpointStructs: move tests to separate file

--- evm/test/Manager.t.sol
+++ evm/test/Manager.t.sol
@@ -518,61 +518,6 @@ contract TestManager is Test, IManagerEvents, IRateLimiterEvents {
     // that way the functionality could be tested separately (and the contracts
     // would also be more reusable)
 
-    // === message encoding/decoding
-
-    // TODO: add some negative tests for unknown message types etc
-
-    function test_SerdeRoundtrip_ManagerMessage(EndpointStructs.ManagerMessage memory m) public {
-        bytes memory message = EndpointStructs.encodeManagerMessage(m);
-
-        EndpointStructs.ManagerMessage memory parsed = EndpointStructs.parseManagerMessage(message);
-
-        assertEq(m.sequence, parsed.sequence);
-        assertEq(m.sender, parsed.sender);
-        assertEq(m.payload, parsed.payload);
-    }
-
-    function test_SerdeJunk_ManagerMessage(EndpointStructs.ManagerMessage memory m) public {
-        bytes memory message = EndpointStructs.encodeManagerMessage(m);
-
-        bytes memory junk = "junk";
-
-        vm.expectRevert(
-            abi.encodeWithSignature(
-                "LengthMismatch(uint256,uint256)", message.length + junk.length, message.length
-            )
-        );
-        EndpointStructs.parseManagerMessage(abi.encodePacked(message, junk));
-    }
-
-    function test_SerdeRoundtrip_NativeTokenTransfer(EndpointStructs.NativeTokenTransfer memory m)
-        public
-    {
-        bytes memory message = EndpointStructs.encodeNativeTokenTransfer(m);
-
-        EndpointStructs.NativeTokenTransfer memory parsed =
-            EndpointStructs.parseNativeTokenTransfer(message);
-
-        assertEq(m.amount.getAmount(), parsed.amount.getAmount());
-        assertEq(m.to, parsed.to);
-        assertEq(m.toChain, parsed.toChain);
-    }
-
-    function test_SerdeJunk_NativeTokenTransfer(EndpointStructs.NativeTokenTransfer memory m)
-        public
-    {
-        bytes memory message = EndpointStructs.encodeNativeTokenTransfer(m);
-
-        bytes memory junk = "junk";
-
-        vm.expectRevert(
-            abi.encodeWithSignature(
-                "LengthMismatch(uint256,uint256)", message.length + junk.length, message.length
-            )
-        );
-        EndpointStructs.parseNativeTokenTransfer(abi.encodePacked(message, junk));
-    }
-
     // === storage
 
     function test_noAutomaticSlot() public {
