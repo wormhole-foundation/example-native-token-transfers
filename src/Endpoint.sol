1feaf422c408101951d92ceb9add187c83a7a4cd
Rahul Maganti <rahulmaganti@rahuls-mbp.mynetworksettings.com>
2024-02-13 13:26:17 -0500

    restructure pausing based on inheritance changes

--- src/Endpoint.sol
+++ src/Endpoint.sol
@@ -20,9 +20,4 @@ abstract contract Endpoint is Pausable {
         pure
         virtual
         returns (EndpointStructs.EndpointMessage memory endpointMessage);
-
-    /// @notice pause the endpoint
-    function _pauseEndpoint() internal {
-        _pause();
-    }
 }
