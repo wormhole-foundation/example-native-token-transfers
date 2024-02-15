ac19e7afb676ff297a2352aeaa39ffb06ca0ca95
Rahul Maganti <rahulmaganti47@gmail.com>
2024-02-15 12:57:33 -0500

    implement configurable pauser role (#98)
    * Pausable: implement Pausable abstract contract
    
    * WormholeEndpointStandalone: add pausing functionality
    
    * Pausable: use custom errors instead of require
    
    * feat: add pausing to WormholeEndpoint, WormholeEndpointAndManager, and WormholeEndpointStandalone
    
    * Pausable: remove superfluous comments
    
    * Pausable: rmv leading underscore on [`isPaused`]
    
    * WormholeEndpointStandalone: rmv outdated doctag
    
    * Pausable: move to libraries
    
    * Endpoint: abstract Pausing functionality out to all endpoints
    
    * EndpointStandalone: expose pausing through the Endpoint Standalone API
    
    * restructure pausing based on inheritance changes
    
    * Pausable: add configurable pauser role
    
    * Pausable: make pauser public view
    
    * Feat: allow owner or pauser role to pause
    
    * Manager/EndpointStandalone: add initialization
    
    * Pausable: fix typo in comment
    
    * fix: modify structure to inherit PausableOwnable to linearize access controls
    
    * fix: make renounce and transfer public
    
    * fix: remove redundant OwnableUpgradeable inheritance from Manager
    
    * fix: rename Pausable.sol to PausableUpgradeable.sol
    
    ---------
    
    Co-authored-by: Rahul Maganti <rahulmaganti@rahuls-mbp.mynetworksettings.com>

---
+++ src/libraries/PausableOwnable.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity >=0.8.8 <0.9.0;
+
+import "./PausableUpgradeable.sol";
+import "./external/OwnableUpgradeable.sol";
+
+abstract contract PausableOwnable is PausableUpgradeable, OwnableUpgradeable {
+    /*
+     * @dev Modifier to allow only the Pauser and the Owner to access pausing functionality
+     */
+    modifier onlyOwnerOrPauser() {
+        _checkOwnerOrPauser(owner());
+        _;
+    }
+
+    /*
+     * @dev Modifier to allow only the Pauser to access some functionality
+     */
+    function _checkOwnerOrPauser(address owner) internal view {
+        if (pauser() != msg.sender && owner != msg.sender) {
+            revert InvalidPauser(msg.sender);
+        }
+    }
+
+    function __PausedOwnable_init(address initialPauser, address owner) internal onlyInitializing {
+        __Paused_init(initialPauser);
+        __Ownable_init(owner);
+    }
+
+    /**
+     * @dev Transfers the ability to pause to a new account (`newPauser`).
+     */
+    function transferPauserCapability(address newPauser) public virtual onlyOwnerOrPauser {
+        PauserStorage storage $ = _getPauserStorage();
+        address oldPauser = $._pauser;
+        $._pauser = newPauser;
+        emit PauserTransferred(oldPauser, newPauser);
+    }
+
+    /**
+     * @dev Leaves the contract without a Pauser
+     */
+    function renouncePauser() public virtual onlyOwnerOrPauser {
+        // NOTE: Cannot renounce the pauser capability when the contract is in the `PAUSED` state
+        // the contract can never be `UNPAUSED`
+        if (isPaused()) {
+            revert CannotRenounceWhilePaused(pauser());
+        }
+        transferPauserCapability(address(0));
+    }
+}
