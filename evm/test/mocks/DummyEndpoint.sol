6e489c110e0c631862432bc7b059c3d50de01570
Rahul Maganti <rahulmaganti47@gmail.com>
2024-02-14 22:31:28 -0500

    Refactor test suite  (#110)
    * Chore: refactor test suite
    
    * Chore: move rate limiting tests out to new module
    
    * Manager.t: use [`setup_endpoints`] method encapsulated in library
    
    * Chore: abstract set limits to separate function
    
    * fix: fix conflicts with base branch
    
    ---------
    
    Co-authored-by: Rahul Maganti <rahulmaganti@rahuls-mbp.mynetworksettings.com>

---
+++ evm/test/mocks/DummyEndpoint.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache 2
+
+pragma solidity >=0.8.8 <0.9.0;
+
+import "forge-std/Test.sol";
+import "../../src/EndpointStandalone.sol";
+import "../interfaces/IEndpointReceiver.sol";
+
+contract DummyEndpoint is EndpointStandalone, IEndpointReceiver {
+    uint16 constant SENDING_CHAIN_ID = 1;
+    bytes4 constant TEST_ENDPOINT_PAYLOAD_PREFIX = 0x99455454;
+
+    constructor(address manager) EndpointStandalone(manager) {}
+
+    function _quoteDeliveryPrice(uint16 /* recipientChain */ )
+        internal
+        pure
+        override
+        returns (uint256)
+    {
+        return 0;
+    }
+
+    function _sendMessage(
+        uint16 recipientChain,
+        uint256 deliveryPayment,
+        bytes memory payload
+    ) internal override {
+        // do nothing
+    }
+
+    function receiveMessage(bytes memory encodedMessage) external {
+        EndpointStructs.EndpointMessage memory parsedEndpointMessage;
+        EndpointStructs.ManagerMessage memory parsedManagerMessage;
+        (parsedEndpointMessage, parsedManagerMessage) = EndpointStructs
+            .parseEndpointAndManagerMessage(TEST_ENDPOINT_PAYLOAD_PREFIX, encodedMessage);
+        _deliverToManager(
+            SENDING_CHAIN_ID, parsedEndpointMessage.sourceManagerAddress, parsedManagerMessage
+        );
+    }
+
+    function parseMessageFromLogs(Vm.Log[] memory logs)
+        public
+        pure
+        returns (uint16 recipientChain, bytes memory payload)
+    {}
+}
