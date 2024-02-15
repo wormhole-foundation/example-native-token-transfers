950353dad4aaf0e59d4194bbc99b899dd621dad6
Rahul Maganti <rahulmaganti47@gmail.com>
2024-02-14 21:12:39 -0500

    Normalize amounts correctly based on number of decimals (#102)
    * Fix: add decimals to [`NormalizedAmount`]
    
    * modify rate limiting logic to support normalized amounts with decimals
    
    * foundry_toml: enable via_ir
    
    * Move NormalizedAmount test to Normalized.t.sol
    
    * NormalizedAmount: rmv redundant underflow check
    
    * Fix: use constructors instead of wrap
    
    * NormalizedAmount: add checks for # of decimals to arithmetic operators
    
    * NormalizedAmount.t: add unit tests for normalization
    
    * EndpointStructs: add numDecimals to wire format comment
    
    * disable via_ir and use block scopes to reduce stack variables
    
    * NormalizedAmount: change minDecimals -> minUint8
    
    * NormalizedAmount: fix normalization edge case (toDecimals < 8 &&
    fromDecimals >= 8)
    
    * Fix: fix conflicts
    
    * EndpointStructs: update wire format docstring
    
    ---------
    
    Co-authored-by: Rahul Maganti <rahulmaganti@rahuls-mbp.mynetworksettings.com>

--- src/libraries/NormalizedAmount.sol
+++ src/libraries/NormalizedAmount.sol
@@ -1,58 +1,172 @@
 // SPDX-License-Identifier: Apache 2
+/// @dev NormalizedAmount is a utility library to handle token amounts with different decimals
 pragma solidity >=0.8.8 <0.9.0;
 
-type NormalizedAmount is uint64;
-
-using {gt as >, lt as <, sub as -, add as +, div, min, unwrap} for NormalizedAmount global;
-
-function gt(NormalizedAmount a, NormalizedAmount b) pure returns (bool) {
-    return NormalizedAmount.unwrap(a) > NormalizedAmount.unwrap(b);
+struct NormalizedAmount {
+    uint64 amount;
+    uint8 decimals;
 }
 
-function lt(NormalizedAmount a, NormalizedAmount b) pure returns (bool) {
-    return NormalizedAmount.unwrap(a) < NormalizedAmount.unwrap(b);
-}
-
-function sub(NormalizedAmount a, NormalizedAmount b) pure returns (NormalizedAmount) {
-    return NormalizedAmount.wrap(NormalizedAmount.unwrap(a) - NormalizedAmount.unwrap(b));
-}
-
-function add(NormalizedAmount a, NormalizedAmount b) pure returns (NormalizedAmount) {
-    return NormalizedAmount.wrap(NormalizedAmount.unwrap(a) + NormalizedAmount.unwrap(b));
-}
-
-function div(NormalizedAmount a, uint64 b) pure returns (NormalizedAmount) {
-    return NormalizedAmount.wrap(NormalizedAmount.unwrap(a) / b);
-}
-
-function min(NormalizedAmount a, NormalizedAmount b) pure returns (NormalizedAmount) {
-    return a > b ? b : a;
-}
-
-function unwrap(NormalizedAmount a) pure returns (uint64) {
-    return NormalizedAmount.unwrap(a);
+function minUint8(uint8 a, uint8 b) pure returns (uint8) {
+    return a < b ? a : b;
 }
 
 library NormalizedAmountLib {
-    error AmountTooLarge(uint256 amount);
+    uint8 constant NORMALIZED_DECIMALS = 8;
 
-    function normalize(uint256 amount, uint8 decimals) internal pure returns (NormalizedAmount) {
-        if (decimals > 8) {
-            amount /= 10 ** (decimals - 8);
-        }
-        // amount after normalization must fit into uint64 (that's the point of
-        // normalization, as Solana only supports uint64 for token amounts)
-        if (amount > type(uint64).max) {
-            revert AmountTooLarge(amount);
-        }
-        return NormalizedAmount.wrap(uint64(amount));
+    error AmountTooLarge(uint256 amount);
+    error NumberOfDecimalsNotEqual(uint8 decimals, uint8 decimalsOther);
+
+    function unwrap(NormalizedAmount memory a) internal pure returns (uint64, uint8) {
+        return (a.amount, a.decimals);
     }
 
-    function denormalize(NormalizedAmount amount, uint8 decimals) internal pure returns (uint256) {
-        uint256 denormalized = NormalizedAmount.unwrap(amount);
-        if (decimals > 8) {
-            denormalized *= 10 ** (decimals - 8);
+    function getAmount(NormalizedAmount memory a) internal pure returns (uint64) {
+        return a.amount;
+    }
+
+    function getDecimals(NormalizedAmount memory a) internal pure returns (uint8) {
+        return a.decimals;
+    }
+
+    function eq(
+        NormalizedAmount memory a,
+        NormalizedAmount memory b
+    ) internal pure returns (bool) {
+        return a.amount == b.amount && a.decimals == b.decimals;
+    }
+
+    function gt(
+        NormalizedAmount memory a,
+        NormalizedAmount memory b
+    ) internal pure returns (bool) {
+        // on initialization
+        if (isZero(b) && !isZero(a)) {
+            return true;
         }
-        return denormalized;
+        if (isZero(a) && !isZero(b)) {
+            return false;
+        }
+
+        if (a.decimals != b.decimals) {
+            revert NumberOfDecimalsNotEqual(a.decimals, b.decimals);
+        }
+
+        return a.amount > b.amount;
+    }
+
+    function lt(
+        NormalizedAmount memory a,
+        NormalizedAmount memory b
+    ) internal pure returns (bool) {
+        // on initialization
+        if (isZero(b) && !isZero(a)) {
+            return false;
+        }
+        if (isZero(a) && !isZero(b)) {
+            return true;
+        }
+
+        if (a.decimals != b.decimals) {
+            revert NumberOfDecimalsNotEqual(a.decimals, b.decimals);
+        }
+
+        return a.amount < b.amount;
+    }
+
+    function isZero(NormalizedAmount memory a) internal pure returns (bool) {
+        return (a.amount == 0 && a.decimals == 0);
+    }
+
+    function sub(
+        NormalizedAmount memory a,
+        NormalizedAmount memory b
+    ) internal pure returns (NormalizedAmount memory) {
+        // on initialization
+        if (isZero(b)) {
+            return a;
+        }
+
+        if (a.decimals != b.decimals) {
+            revert NumberOfDecimalsNotEqual(a.decimals, b.decimals);
+        }
+
+        return NormalizedAmount(a.amount - b.amount, a.decimals);
+    }
+
+    function add(
+        NormalizedAmount memory a,
+        NormalizedAmount memory b
+    ) internal pure returns (NormalizedAmount memory) {
+        // on initialization
+        if (isZero(a)) {
+            return b;
+        }
+
+        if (isZero(b)) {
+            return a;
+        }
+
+        if (a.decimals != b.decimals) {
+            revert NumberOfDecimalsNotEqual(a.decimals, b.decimals);
+        }
+        return NormalizedAmount(a.amount + b.amount, a.decimals);
+    }
+
+    function min(
+        NormalizedAmount memory a,
+        NormalizedAmount memory b
+    ) public pure returns (NormalizedAmount memory) {
+        // on initialization
+        if (isZero(a) && !isZero(b)) {
+            return a;
+        }
+        if (isZero(b) && !isZero(a)) {
+            return b;
+        }
+
+        if (a.decimals != b.decimals) {
+            revert NumberOfDecimalsNotEqual(a.decimals, b.decimals);
+        }
+
+        return a.amount < b.amount ? a : b;
+    }
+
+    /// @dev scale the amount from origDecimals to normDecimals (base 10)
+    function scale(
+        uint256 amount,
+        uint8 fromDecimals,
+        uint8 toDecimals
+    ) internal pure returns (uint256) {
+        if (fromDecimals > toDecimals) {
+            return amount / (10 ** (fromDecimals - toDecimals));
+        } else {
+            return amount * (10 ** (toDecimals - fromDecimals));
+        }
+    }
+
+    function normalize(
+        uint256 amt,
+        uint8 fromDecimals
+    ) internal pure returns (NormalizedAmount memory) {
+        uint8 toDecimals = minUint8(NORMALIZED_DECIMALS, fromDecimals);
+        uint256 amountScaled = scale(amt, fromDecimals, toDecimals);
+
+        // NOTE: amt after normalization must fit into uint64 (that's the point of
+        // normalization, as Solana only supports uint64 for token amts)
+        if (amountScaled > type(uint64).max) {
+            revert AmountTooLarge(amt);
+        }
+        return NormalizedAmount(uint64(amountScaled), toDecimals);
+    }
+
+    function denormalize(
+        NormalizedAmount memory amt,
+        uint8 toDecimals
+    ) internal pure returns (uint256) {
+        (uint256 deNorm, uint8 fromDecimals) = unwrap(amt);
+        uint256 amountScaled = scale(deNorm, fromDecimals, toDecimals);
+
+        return amountScaled;
     }
 }
