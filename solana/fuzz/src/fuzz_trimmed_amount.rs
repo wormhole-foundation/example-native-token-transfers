a9f648f8bc42e1547c7384b1567ddfb45db4b9f6
John Saigle <john@asymmetric.re>
2024-03-06 15:35:18 -0500

    solana: improve fuzzing efficiency

--- solana/fuzz/src/fuzz_trimmed_amount.rs
+++ solana/fuzz/src/fuzz_trimmed_amount.rs
@@ -1,24 +1,13 @@
 use honggfuzz::fuzz;
 use ntt_messages::trimmed_amount::TrimmedAmount;
 
+// #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+
 fn main() {
     loop {
-        fuzz!(|data: &[u8]| {
-            // Manually discard data that doesn't fit our inputs.
-            if data.len() != 10 {
-                return;
-            }
+         fuzz!(|input: (u64, u8, u8)| {
 
-            //  Convert first 8 bytes into a u64.
-            let mut bytes = [0; 8];
-            bytes.copy_from_slice(&data[0..8]);
-            let amount = u64::from_le_bytes(bytes);
-
-            // Use final two bytes as from and to decimals
-            let from = data[8];
-            let to = data[9];
-
-            let _ = TrimmedAmount::trim(amount, from, to);
+            let _ = TrimmedAmount::trim(input.0, input.1, input.2);
         });
     }
 }
