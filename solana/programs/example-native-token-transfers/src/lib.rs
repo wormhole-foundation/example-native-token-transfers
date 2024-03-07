25c8549fc72bf0ed9b65d21cc69e6f1980ba7b64
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2024-03-07 11:09:42 +0000

    solana/quoter: standardise feature flags

--- solana/programs/example-native-token-transfers/src/lib.rs
+++ solana/programs/example-native-token-transfers/src/lib.rs
@@ -27,10 +27,10 @@ use transceivers::wormhole::instructions::*;
 use instructions::*;
 
 cfg_if::cfg_if! {
-    if #[cfg(feature = "tilt-devnet")] {
-        declare_id!("NTTManager111111111111111111111111111111111");
-    } else if #[cfg(feature = "tilt-devnet2")] {
+    if #[cfg(feature = "tilt-devnet2")] {
         declare_id!("NTTManager222222222222222222222222222222222");
+    } else if #[cfg(feature = "tilt-devnet")] {
+        declare_id!("NTTManager111111111111111111111111111111111");
     } else {
         declare_id!("nttiK1SepaQt6sZ4WGW5whvc9tEnGXGxuKeptcQPCcS");
     }
