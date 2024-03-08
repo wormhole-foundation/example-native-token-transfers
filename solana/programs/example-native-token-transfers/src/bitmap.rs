32481bac974c310488d8e0d1426e643810c62e30
John Saigle <john@asymmetric.re>
2024-03-08 12:33:29 -0500

    solana: Configure clippy to deny possible truncation
    Adds a clippy rule to block the inclusion of code that can result in
    truncation (i.e. `as T` conversions)
    Adds linting directive to existing instances in the code and add
    comments about why they are safe
    Add a unit test on bitmap length to confirm that the maximum possible
    number of votes (i.e. maximum "length" of the bitmap) is within the
    limits of u8.

--- solana/programs/example-native-token-transfers/src/bitmap.rs
+++ solana/programs/example-native-token-transfers/src/bitmap.rs
@@ -31,8 +31,11 @@ impl Bitmap {
         BM::<128>::from_value(self.map).get(index as usize)
     }
 
+    #[allow(clippy::cast_possible_truncation)]
     pub fn count_enabled_votes(&self, enabled: Bitmap) -> u8 {
         let bm = BM::<128>::from_value(self.map) & BM::<128>::from_value(enabled.map);
+        // Conversion from usize to u8 is safe here. The Bitmap uses u128, so it's maximum length
+        // (number of true bits) is 128.
         bm.len() as u8
     }
 }
@@ -63,5 +66,11 @@ mod tests {
 
         enabled.set(18, false);
         assert_eq!(bm.count_enabled_votes(enabled), 1);
+    }
+
+    #[test]
+    fn test_bitmap_len() {
+        let max_bitmap = Bitmap::from_value(u128::MAX);
+        assert_eq!(128, max_bitmap.count_enabled_votes(max_bitmap));
     }
 }
