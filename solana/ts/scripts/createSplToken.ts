8a07f6aafe0c25fdaa776408186a3f8b6d33957a
solanoe <solanoepalacio@gmail.com>
2024-03-18 18:06:01 -0300

    adds ledger signer to create-spl-token script as poc

--- solana/ts/scripts/createSplToken.ts
+++ solana/ts/scripts/createSplToken.ts
@@ -2,10 +2,12 @@ import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMint2Instruction } from "@
 import {
   SystemProgram,
   Keypair,
+  PublicKey,
+  Transaction,
 } from "@solana/web3.js";
 
-import { connection, deployerKeypair } from "./env";
-import { buildTransaction } from "./helpers";
+import { connection, getSigner } from "./env";
+import { ledgerSignAndSend } from "./helpers";
 
 const mintKeypair = Keypair.generate();
 
@@ -18,78 +20,27 @@ console.log("Creating mint account with keypair:", mintKeypair.publicKey.toBase5
     symbol: "TEST_NTT_TKN",
     uri: "https://thisisnot.arealurl/info.json",
   };
+  const signer = await getSigner();
+  const signerPk = new PublicKey(await signer.getAddress());
 
-  const createMintAccountInstruction = SystemProgram.createAccount({
-    fromPubkey: deployerKeypair.publicKey,
+  const createAccountIx = SystemProgram.createAccount({
+    fromPubkey: signerPk,
     newAccountPubkey: mintKeypair.publicKey,
     space: MINT_SIZE,
     lamports: await connection.getMinimumBalanceForRentExemption(MINT_SIZE),
-    programId: TOKEN_PROGRAM_ID,
+    programId: TOKEN_PROGRAM_ID
   });
 
-  const initializeMintInstruction = createInitializeMint2Instruction(
+  const initMintIx = createInitializeMint2Instruction(
     mintKeypair.publicKey,
     tokenConfig.decimals,
-    deployerKeypair.publicKey,
-    null,
+    signerPk,
+    signerPk, //signerPk,
+    TOKEN_PROGRAM_ID,
   );
 
-  // const accountMetadataInstruction = getCreateMetadataAccountInstruction(tokenConfig);
+  const tx = new Transaction().add(createAccountIx, initMintIx);
 
-  const tx = await buildTransaction({
-    connection,
-    payer: deployerKeypair.publicKey,
-    signers: [deployerKeypair, mintKeypair],
-    instructions: [
-      createMintAccountInstruction,
-      initializeMintInstruction,
-      // accountMetadataInstruction,
-    ],
-  });
-
-
-  try {
-    const sig = await connection.sendTransaction(tx);
-  } catch (err) {
-    console.error("Failed to send transaction:");
-    console.log(tx);
-
-    throw err;
-  }
+  return ledgerSignAndSend(tx, signer, [mintKeypair]);
 })();
-
-// TODO: Finish this function to add account metadata to the token
-// function getCreateMetadataAccountInstruction(tokenConfig) {
-//   // const metadataAccount = PublicKey.findProgramAddressSync(
-//   //   [Buffer.from("metadata"), METADATA_PROGRAM_ID.toBuffer(), mintKeypair.publicKey.toBuffer()],
-//   //   METADATA_PROGRAM_ID,
-//   // )[0];
-
-//   // console.log("Metadata address:", metadataAccount.toBase58());
-
-//   const createAccountArgs = {
-//     // metadata: metadataAccount,
-//     mint: mintKeypair.publicKey,
-//     mintAuthority: deployerKeypair.publicKey,
-//     payer: deployerKeypair.publicKey,
-//     updateAuthority: deployerKeypair.publicKey,
-//     // rent?
-//     data: {
-//       creators: null,
-//       name: tokenConfig.name,
-//       symbol: tokenConfig.symbol,
-//       uri: tokenConfig.uri,
-//       sellerFeeBasisPoints: 0,
-//       collection: null,
-//       uses: null,
-//     },
-//     // `collectionDetails` - for non-nft type tokens, normally set to `null` to not have a value set
-//     collectionDetails: null,
-//     isMutable: false,
-//   };
-
-//   const context = createUmi(rpcUrl);
-
-//   return createMetadataAccountV3(context, createAccountArgs);
-// }
 
