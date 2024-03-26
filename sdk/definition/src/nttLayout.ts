72e9ced30755cbb5a48316e5023269da3b2f80db
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-03-26 16:57:42 -0400

    setup for tests, install defs from local packed until a new version is published

--- sdk/definition/src/nttLayout.ts
+++ sdk/definition/src/nttLayout.ts
@@ -1,4 +1,8 @@
-import type { CustomizableBytes, Layout, LayoutToType } from "@wormhole-foundation/sdk-base";
+import type {
+  CustomizableBytes,
+  Layout,
+  LayoutToType,
+} from "@wormhole-foundation/sdk-base";
 import { customizableBytes } from "@wormhole-foundation/sdk-base";
 
 import {
@@ -18,7 +22,12 @@ export type TrimmedAmount = LayoutToType<typeof trimmedAmountLayout>;
 export type Prefix = readonly [number, number, number, number];
 
 const prefixItem = (prefix: Prefix) =>
-  ({ name: "prefix", binary: "bytes", custom: Uint8Array.from(prefix), omit: true }) as const;
+  ({
+    name: "prefix",
+    binary: "bytes",
+    custom: Uint8Array.from(prefix),
+    omit: true,
+  } as const);
 
 export const nativeTokenTransferLayout = [
   prefixItem([0x99, 0x4e, 0x54, 0x54]),
@@ -28,31 +37,41 @@ export const nativeTokenTransferLayout = [
   { name: "recipientChain", ...layoutItems.chainItem() }, //TODO restrict to supported chains?
 ] as const satisfies Layout;
 
-export type NativeTokenTransfer = LayoutToType<typeof nativeTokenTransferLayout>;
+export type NativeTokenTransfer = LayoutToType<
+  typeof nativeTokenTransferLayout
+>;
 
 export const transceiverMessageLayout = <
   const MP extends CustomizableBytes = undefined,
-  const TP extends CustomizableBytes = undefined,
+  const TP extends CustomizableBytes = undefined
 >(
   prefix: Prefix,
   nttManagerPayload?: MP,
-  transceiverPayload?: TP,
+  transceiverPayload?: TP
 ) =>
   [
     prefixItem(prefix),
     { name: "sourceNttManager", ...layoutItems.universalAddressItem },
     { name: "recipientNttManager", ...layoutItems.universalAddressItem },
-    customizableBytes({ name: "nttManagerPayload", lengthSize: 2 }, nttManagerPayload),
-    customizableBytes({ name: "transceiverPayload", lengthSize: 2 }, transceiverPayload),
+    customizableBytes(
+      { name: "nttManagerPayload", lengthSize: 2 },
+      nttManagerPayload
+    ),
+    customizableBytes(
+      { name: "transceiverPayload", lengthSize: 2 },
+      transceiverPayload
+    ),
   ] as const satisfies Layout;
 
 export type TransceiverMessage<
   MP extends CustomizableBytes = undefined,
-  TP extends CustomizableBytes = undefined,
+  TP extends CustomizableBytes = undefined
 > = LayoutToType<ReturnType<typeof transceiverMessageLayout<MP, TP>>>;
 
-export const nttManagerMessageLayout = <const P extends CustomizableBytes = undefined>(
-  customPayload?: P,
+export const nttManagerMessageLayout = <
+  const P extends CustomizableBytes = undefined
+>(
+  customPayload?: P
 ) =>
   [
     { name: "id", binary: "bytes", size: 32 },
@@ -60,24 +79,32 @@ export const nttManagerMessageLayout = <const P extends CustomizableBytes = unde
     customizableBytes({ name: "payload", lengthSize: 2 }, customPayload),
   ] as const satisfies Layout;
 
-export type NttManagerMessage<P extends CustomizableBytes = undefined> = LayoutToType<
-  ReturnType<typeof nttManagerMessageLayout<P>>
->;
+export type NttManagerMessage<P extends CustomizableBytes = undefined> =
+  LayoutToType<ReturnType<typeof nttManagerMessageLayout<P>>>;
 
-export const wormholeTransceiverMessageLayout = <MP extends CustomizableBytes = undefined>(
-  nttManagerPayload?: MP,
-) => transceiverMessageLayout([0x99, 0x45, 0xff, 0x10], nttManagerPayload, new Uint8Array(0));
+export const wormholeTransceiverMessageLayout = <
+  MP extends CustomizableBytes = undefined
+>(
+  nttManagerPayload?: MP
+) =>
+  transceiverMessageLayout(
+    [0x99, 0x45, 0xff, 0x10],
+    nttManagerPayload,
+    new Uint8Array(0)
+  );
 
-export type WormholeTransceiverMessage<MP extends CustomizableBytes = undefined> = LayoutToType<
-  ReturnType<typeof wormholeTransceiverMessageLayout<MP>>
->;
+export type WormholeTransceiverMessage<
+  MP extends CustomizableBytes = undefined
+> = LayoutToType<ReturnType<typeof wormholeTransceiverMessageLayout<MP>>>;
 
 const wormholeNativeTokenTransferLayout = wormholeTransceiverMessageLayout(
-  nttManagerMessageLayout(nativeTokenTransferLayout),
+  nttManagerMessageLayout(nativeTokenTransferLayout)
 );
 
-export const transceiverInstructionLayout = <const P extends CustomizableBytes = undefined>(
-  customPayload?: P,
+export const transceiverInstructionLayout = <
+  const P extends CustomizableBytes = undefined
+>(
+  customPayload?: P
 ) =>
   [
     { name: "index", binary: "uint", size: 1 },
