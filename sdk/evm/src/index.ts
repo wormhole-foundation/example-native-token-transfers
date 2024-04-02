2acd5815762692be17280e6f79b98835bad9c1fb
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-04-02 08:05:53 -0400

    SDK: Add ts sdk

---
+++ sdk/evm/src/index.ts
@@ -0,0 +1,9 @@
+import { registerProtocol } from "@wormhole-foundation/sdk-connect";
+import { _platform } from "@wormhole-foundation/sdk-evm";
+import { EvmNtt } from "./ntt.js";
+import "@wormhole-foundation/sdk-definitions-ntt";
+
+registerProtocol(_platform, "Ntt", EvmNtt);
+
+export * as ethers_contracts from "./ethers-contracts/index.js";
+export * from "./ntt.js";
