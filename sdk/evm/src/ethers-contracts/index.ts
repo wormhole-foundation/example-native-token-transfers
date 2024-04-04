2a0f1a10543a9b0fc2e5b041ae21287cdaa5e097
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-04-04 08:08:24 -0400

    SDK: Versioned ABI  (#382)

---
+++ sdk/evm/src/ethers-contracts/index.ts
@@ -0,0 +1,13 @@
+import * as __0_1_0 from "./0_1_0/index.js";
+export type * from "./0_1_0/index.js";
+
+const _0_1_0 = {
+  NttManager: {
+    connect: __0_1_0.NttManager__factory.connect,
+  },
+  NttTransceiver: {
+    connect: __0_1_0.WormholeTransceiver__factory.connect,
+  },
+};
+
+export { _0_1_0 };
