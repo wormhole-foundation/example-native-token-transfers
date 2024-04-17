42f56844b02935e6e61256fe17ec7f34d19b836f
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-04-17 15:02:06 -0400

    sending from sol

--- sdk/examples/src/index.ts
+++ sdk/examples/src/index.ts
@@ -1,14 +1,20 @@
-import { wormhole } from "@wormhole-foundation/sdk";
-import solana from "@wormhole-foundation/sdk/solana";
+import { signSendWait, wormhole } from "@wormhole-foundation/sdk";
 import evm from "@wormhole-foundation/sdk/evm";
+import solana from "@wormhole-foundation/sdk/solana";
 
 import "@wormhole-foundation/sdk-definitions-ntt";
 import "@wormhole-foundation/sdk-evm-ntt";
 import "@wormhole-foundation/sdk-solana-ntt";
+import { getSigner } from "./helpers.js";
 
 (async function () {
   const wh = await wormhole("Testnet", [solana, evm]);
   const sol = wh.getChain("Solana");
+  const arb = wh.getChain("ArbitrumSepolia");
+
+  const sendSigner = await getSigner(sol);
+  const rcvSigner = await getSigner(arb);
+
   const originNtt = await sol.getProtocol("Ntt", {
     ...sol.config.contracts,
     ntt: {
@@ -21,4 +27,14 @@ import "@wormhole-foundation/sdk-solana-ntt";
   });
 
   console.log(await originNtt.getCurrentOutboundCapacity());
+  console.log(await originNtt.getCurrentInboundCapacity("ArbitrumSepolia"));
+
+  const xfer = originNtt.transfer(
+    sendSigner.address.address,
+    1000n,
+    rcvSigner.address,
+    false
+  );
+
+  console.log(await signSendWait(sol, xfer, sendSigner.signer));
 })();
