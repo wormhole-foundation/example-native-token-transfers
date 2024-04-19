9ecbd97981fa0c19f2737754d2f1677dcac51327
Csongor Kiss <kiss.csongor.kiss@gmail.com>
2024-04-19 14:14:13 +0100

    solana: populate an address lookup table to reduce tx size

--- solana/programs/example-native-token-transfers/src/instructions/mod.rs
+++ solana/programs/example-native-token-transfers/src/instructions/mod.rs
@@ -1,11 +1,13 @@
 pub mod admin;
 pub mod initialize;
+pub mod luts;
 pub mod redeem;
 pub mod release_inbound;
 pub mod transfer;
 
 pub use admin::*;
 pub use initialize::*;
+pub use luts::*;
 pub use redeem::*;
 pub use release_inbound::*;
 pub use transfer::*;
