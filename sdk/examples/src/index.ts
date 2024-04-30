f07757db172d7451fb4646156ae4a0ac461e210f
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-04-30 13:48:37 -0400

    add spl22 token config, test both directions

--- sdk/examples/src/index.ts
+++ sdk/examples/src/index.ts
@@ -10,45 +10,38 @@ import solana from "@wormhole-foundation/sdk/platforms/solana";
 import "@wormhole-foundation/sdk-evm-ntt";
 import "@wormhole-foundation/sdk-solana-ntt";
 
-import { TEST_NTT_TOKENS } from "./consts.js";
+import { TEST_NTT_SPL22_TOKENS, TEST_NTT_TOKENS } from "./consts.js";
 import { getSigner } from "./helpers.js";
 
 // Recover an in-flight transfer by setting txids here from output of previous run
 const recoverTxids: TransactionId[] = [
-  // { chain: "Solana", txid: "hZXRs9TEvMWnSAzcgmrEuHsq1C5rbcompy63vkJ2SrXv4a7u6ZBEaJAkBMXKAfScCooDNhN36Jt4PMcDhN8yGjP", },
+  //{ chain: "Solana", txid: "hZXRs9TEvMWnSAzcgmrEuHsq1C5rbcompy63vkJ2SrXv4a7u6ZBEaJAkBMXKAfScCooDNhN36Jt4PMcDhN8yGjP", },
 ];
 
 (async function () {
   const wh = new Wormhole("Testnet", [solana.Platform, evm.Platform]);
   const src = wh.getChain("Solana");
-  const dst = wh.getChain("ArbitrumSepolia");
+  const dst = wh.getChain("Sepolia");
 
   const srcSigner = await getSigner(src);
   const dstSigner = await getSigner(dst);
 
   const srcNttInit = src.platform.getProtocolInitializer("Ntt");
-  const srcNtt = new srcNttInit(
-    wh.network,
-    src.chain,
-    await src.getRpc(),
-    {
-      ...src.config.contracts,
-      ntt: TEST_NTT_TOKENS[src.chain],
-    },
-    // @ts-ignore
-    "1.0.0"
-  );
+  const srcNtt = new srcNttInit(wh.network, src.chain, await src.getRpc(), {
+    ...src.config.contracts,
+    ntt: TEST_NTT_SPL22_TOKENS[src.chain],
+  });
 
   const dstNttInit = dst.platform.getProtocolInitializer("Ntt");
   const dstNtt = new dstNttInit(wh.network, dst.chain, await dst.getRpc(), {
     ...dst.config.contracts,
-    ntt: TEST_NTT_TOKENS[dst.chain],
+    ntt: TEST_NTT_SPL22_TOKENS[dst.chain],
   });
 
   console.log("Source signer", srcSigner.address.address);
 
   const xfer = () =>
-    srcNtt.transfer(srcSigner.address.address, 1000n, dstSigner.address, {
+    srcNtt.transfer(srcSigner.address.address, 100000n, dstSigner.address, {
       queue: false,
       automatic: false,
       gasDropoff: 0n,
@@ -66,7 +59,7 @@ const recoverTxids: TransactionId[] = [
 
   const dstTxids = await signSendWait(
     dst,
-    dstNtt.redeem([vaa!]),
+    dstNtt.redeem([vaa!], dstSigner.address.address),
     dstSigner.signer
   );
   console.log("dstTxids", dstTxids);
