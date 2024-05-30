b2ad51803cda82551b9ad42a6662390327eb653d
Nikhil Suri <nikhilsuri@comcast.net>
2024-05-30 10:31:37 -0700

    Update NttManager.sol version

--- evm/src/NttManager/NttManager.sol
+++ evm/src/NttManager/NttManager.sol
@@ -41,7 +41,7 @@ contract NttManager is INttManager, RateLimiter, ManagerBase {
     using TrimmedAmountLib for uint256;
     using TrimmedAmountLib for TrimmedAmount;
 
-    string public constant NTT_MANAGER_VERSION = "1.0.0";
+    string public constant NTT_MANAGER_VERSION = "1.1.0";
 
     // =============== Setup =================================================================
 
