1848c3d7ef9031db15e9c41031f64a7466217303
solanoe <solanoepalacio@gmail.com>
2024-06-05 13:10:28 -0300

    solana scripts: execute multiple governance vaas + june-05 governance vaas

---
+++ solana/ts/scripts/executeConvernanceVaas.ts
@@ -0,0 +1,77 @@
+import {
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import { parseVaa } from "@certusone/wormhole-sdk";
+import {
+  connection,
+  getSigner,
+  getProgramAddresses,
+  getGovernanceVaa,
+} from "./env";
+import { addLedgerSignature, ledgerSignAndSend } from "./helpers";
+import { postVaaSolana } from "@certusone/wormhole-sdk";
+import { NTTGovernance } from "../sdk/governance";
+import { loadScriptConfig } from '../../../evm/ts-scripts/src/env';
+
+const governanceVaasFileName = process.env.GOVERNANCE_VAAS_FILE_PATH;
+if (!governanceVaasFileName) {
+  throw new Error("GOVERNANCE_VAAS_FILE_NAME is required.");
+}
+
+(async () => {
+
+  const vaas: { id: string, vaa: string }[] = loadScriptConfig(governanceVaasFileName);
+  const { wormholeProgramId, governanceProgramId } =
+    getProgramAddresses();
+
+  const governance = new NTTGovernance(connection, {
+    programId: governanceProgramId as any,
+  });
+
+  const signer = await getSigner();
+  const signerPk = new PublicKey(await signer.getAddress());
+
+  async function sign(tx: Transaction) {
+    await addLedgerSignature(tx, signer, signerPk);
+    return tx;
+  }
+
+  const instructions: TransactionInstruction[] = [];
+  for (const vaa of vaas) {
+    const vaaBuff = Buffer.from(vaa.vaa, "base64");
+
+    console.log(`Posting VAA ${vaa.id} to Solana...`);
+  
+    await postVaaSolana(
+      connection,
+      sign,
+      new PublicKey(wormholeProgramId),
+      signerPk,
+      vaaBuff,
+    );
+
+    console.log("VAA post succeeded");
+
+    const parsedVaa = parseVaa(vaaBuff);
+
+    const governanceIx = await governance.createGovernanceVaaInstruction({
+      payer: signerPk,
+      vaa: parsedVaa,
+      wormholeId: new PublicKey(wormholeProgramId),
+    });
+
+    instructions.push(governanceIx)
+  }
+
+  console.log(`Executing governance instructions.`);
+
+  const signature = await ledgerSignAndSend(instructions, []);
+
+  console.log("Waiting for confirmation... Signature: ", signature);
+
+  await connection.confirmTransaction(signature);
+
+  console.log("success.");
+})();
