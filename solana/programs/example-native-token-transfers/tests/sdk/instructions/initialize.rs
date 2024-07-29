fa57a59e97c5210c2db6cd9baf6cd249787ca6ab
nvsriram <nuranisri01@gmail.com>
2024-07-29 17:57:38 -0400

    solana: Add *_with_token_program_id helper methods

--- solana/programs/example-native-token-transfers/tests/sdk/instructions/initialize.rs
+++ solana/programs/example-native-token-transfers/tests/sdk/instructions/initialize.rs
@@ -13,6 +13,15 @@ pub struct Initialize {
 }
 
 pub fn initialize(ntt: &NTT, accounts: Initialize, args: InitializeArgs) -> Instruction {
+    initialize_with_token_program_id(ntt, accounts, args, &Token::id())
+}
+
+pub fn initialize_with_token_program_id(
+    ntt: &NTT,
+    accounts: Initialize,
+    args: InitializeArgs,
+    token_program_id: &Pubkey,
+) -> Instruction {
     let data = example_native_token_transfers::instruction::Initialize { args };
 
     let bpf_loader_upgradeable_program = BpfLoaderUpgradeable::id();
@@ -24,8 +33,8 @@ pub fn initialize(ntt: &NTT, accounts: Initialize, args: InitializeArgs) -> Inst
         mint: accounts.mint,
         rate_limit: ntt.outbox_rate_limit(),
         token_authority: ntt.token_authority(),
-        custody: ntt.custody(&accounts.mint),
-        token_program: Token::id(),
+        custody: ntt.custody_with_token_program_id(&accounts.mint, token_program_id),
+        token_program: *token_program_id,
         associated_token_program: AssociatedToken::id(),
         bpf_loader_upgradeable_program,
         system_program: System::id(),
