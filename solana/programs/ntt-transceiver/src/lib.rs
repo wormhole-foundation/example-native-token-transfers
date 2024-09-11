c0931e4b6360bc67bc3f7320b65442536fd540a7
nvsriram <nuranisri01@gmail.com>
2024-09-11 14:04:47 -0400

    solana: Add transceivers as standalone program

---
+++ solana/programs/ntt-transceiver/src/lib.rs
@@ -0,0 +1,45 @@
+use anchor_lang::prelude::*;
+pub mod peer;
+pub mod wormhole;
+
+use wormhole::instructions::*;
+
+declare_id!("Ee6jpX9oq2EsGuqGb6iZZxvtcpmMGZk8SAUbnQy4jcHR");
+
+#[program]
+pub mod ntt_transceiver {
+
+    use super::*;
+
+    pub fn set_wormhole_peer(
+        ctx: Context<SetTransceiverPeer>,
+        args: SetTransceiverPeerArgs,
+    ) -> Result<()> {
+        set_transceiver_peer(ctx, args)
+    }
+
+    pub fn receive_wormhole_message(ctx: Context<ReceiveMessage>) -> Result<()> {
+        wormhole::instructions::receive_message(ctx)
+    }
+
+    pub fn release_wormhole_outbound(
+        ctx: Context<ReleaseOutbound>,
+        args: ReleaseOutboundArgs,
+    ) -> Result<()> {
+        wormhole::instructions::release_outbound(ctx, args)
+    }
+
+    pub fn broadcast_wormhole_id(ctx: Context<BroadcastId>) -> Result<()> {
+        wormhole::instructions::broadcast_id(ctx)
+    }
+
+    pub fn broadcast_wormhole_peer(
+        ctx: Context<BroadcastPeer>,
+        args: BroadcastPeerArgs,
+    ) -> Result<()> {
+        wormhole::instructions::broadcast_peer(ctx, args)
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
---
+++ solana/programs/ntt-transceiver/src/wormhole/instructions/release_outbound.rs
@@ -0,0 +1,100 @@
+use crate::wormhole::accounts::*;
+use anchor_lang::prelude::*;
+use example_native_token_transfers::{
+    error::NTTError, queue::outbox::OutboxItem, registered_transceiver::*, config::*,
+};
+use ntt_messages::{
+    ntt::NativeTokenTransfer, ntt_manager::NttManagerMessage, transceiver::TransceiverMessage,
+    transceivers::wormhole::WormholeTransceiver,
+};
+
+#[derive(Accounts)]
+pub struct ReleaseOutbound<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub config: NotPausedConfig<'info>,
+
+    #[account(
+        mut,
+        constraint = !outbox_item.released.get(transceiver.id)? @ NTTError::MessageAlreadySent,
+    )]
+    pub outbox_item: Account<'info, OutboxItem>,
+
+    #[account(
+        constraint = transceiver.transceiver_address == crate::ID,
+        constraint = config.enabled_transceivers.get(transceiver.id)? @ NTTError::DisabledTransceiver
+    )]
+    pub transceiver: Account<'info, RegisteredTransceiver>,
+
+    #[account(
+        mut,
+        seeds = [b"message", outbox_item.key().as_ref()],
+        bump,
+    )]
+    /// CHECK: initialized and written to by wormhole core bridge
+    pub wormhole_message: UncheckedAccount<'info>,
+
+    #[account(
+        seeds = [b"emitter"],
+        bump
+    )]
+    // TODO: do we want to put anything in here?
+    /// CHECK: wormhole uses this as the emitter address
+    pub emitter: UncheckedAccount<'info>,
+
+    pub wormhole: WormholeAccounts<'info>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ReleaseOutboundArgs {
+    pub revert_on_delay: bool,
+}
+
+pub fn release_outbound(ctx: Context<ReleaseOutbound>, args: ReleaseOutboundArgs) -> Result<()> {
+    let accs = ctx.accounts;
+    let released = accs.outbox_item.try_release(accs.transceiver.id)?;
+
+    if !released {
+        if args.revert_on_delay {
+            return Err(NTTError::CantReleaseYet.into());
+        } else {
+            return Ok(());
+        }
+    }
+
+    assert!(accs.outbox_item.released.get(accs.transceiver.id)?);
+    let message: TransceiverMessage<WormholeTransceiver, NativeTokenTransfer> =
+        TransceiverMessage::new(
+            // TODO: should we just put the ntt id here statically?
+            accs.outbox_item.to_account_info().owner.to_bytes(),
+            accs.outbox_item.recipient_ntt_manager,
+            NttManagerMessage {
+                id: accs.outbox_item.key().to_bytes(),
+                sender: accs.outbox_item.sender.to_bytes(),
+                payload: NativeTokenTransfer {
+                    amount: accs.outbox_item.amount,
+                    source_token: accs.config.mint.to_bytes(),
+                    to: accs.outbox_item.recipient_address,
+                    to_chain: accs.outbox_item.recipient_chain,
+                },
+            },
+            vec![],
+        );
+
+    post_message(
+        &accs.wormhole,
+        accs.payer.to_account_info(),
+        accs.wormhole_message.to_account_info(),
+        accs.emitter.to_account_info(),
+        ctx.bumps.emitter,
+        &message,
+        &[&[
+            b"message",
+            accs.outbox_item.key().as_ref(),
+            &[ctx.bumps.wormhole_message],
+        ]],
+    )?;
+
+    Ok(())
+}
