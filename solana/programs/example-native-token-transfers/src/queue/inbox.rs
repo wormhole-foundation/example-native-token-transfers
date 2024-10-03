0fa8ac201d78f9c3f6c823c2c58e8df933075e96
nvsriram <nuranisri01@gmail.com>
2024-10-03 15:08:40 -0400

    solana: Remove unused `init_if_needed` fn

--- solana/programs/example-native-token-transfers/src/queue/inbox.rs
+++ solana/programs/example-native-token-transfers/src/queue/inbox.rs
@@ -1,12 +1,8 @@
 use std::ops::{Deref, DerefMut};
 
-use anchor_lang::{prelude::*, system_program};
-use ntt_messages::ntt::NativeTokenTransfer;
+use anchor_lang::prelude::*;
 
-use crate::{
-    bitmap::Bitmap, clock::current_timestamp, error::NTTError,
-    messages::ValidatedTransceiverMessage,
-};
+use crate::{bitmap::Bitmap, clock::current_timestamp, error::NTTError};
 
 use super::rate_limit::RateLimitState;
 
@@ -37,142 +33,6 @@ pub enum ReleaseStatus {
 
 impl InboxItem {
     pub const SEED_PREFIX: &'static [u8] = b"inbox_item";
-
-    /// Inits inbox_item if not done so previously
-    /// Returns (deserialized inbox_item, bump)
-    pub fn init_if_needed<'info>(
-        info: &UncheckedAccount<'info>,
-        transceiver_message: &ValidatedTransceiverMessage<NativeTokenTransfer>,
-        payer: &Signer<'info>,
-        system_program: &Program<'info, System>,
-    ) -> Result<(Self, u8)> {
-        let (pda_address, bump) = Pubkey::find_program_address(
-            &[
-                InboxItem::SEED_PREFIX,
-                transceiver_message
-                    .message
-                    .ntt_manager_payload
-                    .keccak256(transceiver_message.from_chain)
-                    .as_ref(),
-            ],
-            &crate::ID,
-        );
-        if info.key() != pda_address {
-            return Err(Error::from(ErrorCode::ConstraintSeeds)
-                .with_account_name("inbox_item")
-                .with_pubkeys((info.key(), pda_address)));
-        }
-        let anchor_rent = Rent::get()?;
-        let space = 8 + InboxItem::INIT_SPACE;
-        let inbox_item: InboxItem = if info.owner == &system_program::ID {
-            let current_lamports = info.lamports();
-            if current_lamports == 0 {
-                let lamports = anchor_rent.minimum_balance(space);
-                system_program::create_account(
-                    CpiContext::new(
-                        system_program.to_account_info(),
-                        system_program::CreateAccount {
-                            from: payer.to_account_info(),
-                            to: info.to_account_info(),
-                        },
-                    )
-                    .with_signer(&[&[
-                        InboxItem::SEED_PREFIX,
-                        transceiver_message
-                            .message
-                            .ntt_manager_payload
-                            .keccak256(transceiver_message.from_chain)
-                            .as_ref(),
-                        &[bump][..],
-                    ][..]]),
-                    lamports,
-                    space as u64,
-                    &crate::ID,
-                )?;
-            } else {
-                if payer.key() == info.key() {
-                    return Err(Error::from(ErrorCode::TryingToInitPayerAsProgramAccount)
-                        .with_pubkeys((payer.key(), info.key())));
-                }
-                let required_lamports = anchor_rent
-                    .minimum_balance(space)
-                    .max(1)
-                    .saturating_sub(current_lamports);
-                if required_lamports > 0 {
-                    system_program::transfer(
-                        CpiContext::new(
-                            system_program.to_account_info(),
-                            system_program::Transfer {
-                                from: payer.to_account_info(),
-                                to: info.to_account_info(),
-                            },
-                        ),
-                        required_lamports,
-                    )?;
-                }
-                system_program::allocate(
-                    CpiContext::new(
-                        system_program.to_account_info(),
-                        system_program::Allocate {
-                            account_to_allocate: info.to_account_info(),
-                        },
-                    )
-                    .with_signer(&[&[
-                        InboxItem::SEED_PREFIX,
-                        transceiver_message
-                            .message
-                            .ntt_manager_payload
-                            .keccak256(transceiver_message.from_chain)
-                            .as_ref(),
-                        &[bump][..],
-                    ][..]]),
-                    space as u64,
-                )?;
-                system_program::assign(
-                    CpiContext::new(
-                        system_program.to_account_info(),
-                        system_program::Assign {
-                            account_to_assign: info.to_account_info(),
-                        },
-                    )
-                    .with_signer(&[&[
-                        InboxItem::SEED_PREFIX,
-                        transceiver_message
-                            .message
-                            .ntt_manager_payload
-                            .keccak256(transceiver_message.from_chain)
-                            .as_ref(),
-                        &[bump][..],
-                    ][..]]),
-                    &crate::ID,
-                )?;
-            }
-            let mut data: &[u8] = &info.try_borrow_data()?;
-            InboxItem::try_deserialize_unchecked(&mut data)?
-        } else {
-            let mut data: &[u8] = &info.try_borrow_data()?;
-            InboxItem::try_deserialize(&mut data)?
-        };
-        if space != info.data_len() {
-            return Err(Error::from(ErrorCode::ConstraintSpace)
-                .with_account_name("inbox_item")
-                .with_values((space, info.data_len())));
-        }
-        if info.owner != &crate::ID {
-            return Err(Error::from(ErrorCode::ConstraintOwner)
-                .with_account_name("inbox_item")
-                .with_pubkeys((*info.owner, crate::ID)));
-        }
-        {
-            let required_lamports = anchor_rent.minimum_balance(space);
-            if info.to_account_info().lamports() < required_lamports {
-                return Err(
-                    Error::from(ErrorCode::ConstraintRentExempt).with_account_name("inbox_item")
-                );
-            }
-        }
-        Ok((inbox_item, bump))
-    }
 
     /// Attempt to release the transfer.
     /// Returns true if the transfer was released, false if it was not yet time to release it.
